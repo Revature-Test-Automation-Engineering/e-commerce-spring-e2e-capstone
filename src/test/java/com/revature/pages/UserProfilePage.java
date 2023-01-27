6c2a66bff96acde430d4b5d4702ed72ed3931583
khavvia <khavvia@gmail.com>
2023-01-27 13:27:04 -0600

    t

--- src/test/java/com/revature/pages/UserProfilePage.java
+++ src/test/java/com/revature/pages/UserProfilePage.java
@@ -1,5 +1,9 @@
 package com.revature.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 public class UserProfilePage {
 
 
