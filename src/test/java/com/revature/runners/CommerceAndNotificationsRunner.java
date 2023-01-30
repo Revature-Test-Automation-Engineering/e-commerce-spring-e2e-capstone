ffac4198113311a89436e768f2d62126408d7ddd
khavvia <khavvia@gmail.com>
2023-01-30 15:51:15 -0600

    finished ViewPreviousOrderImpl

--- src/test/java/com/revature/runners/CommerceAndNotificationsRunner.java
+++ src/test/java/com/revature/runners/CommerceAndNotificationsRunner.java
@@ -1,5 +1,8 @@
 package com.revature.runners;
 
+import com.revature.pages.LoginPage;
+import com.revature.pages.MainPage;
+import com.revature.pages.OrdersPage;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -17,14 +20,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CommerceAndNotificationsRunner {
 
     public static WebDriver driver;
-
-    
+    public static MainPage mainPage;
+    public static LoginPage loginPage;
+    public static OrdersPage ordersPage;
 
 
     @BeforeClass
     public static void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        mainPage = new MainPage(driver);
+        loginPage = new LoginPage(driver);
+        ordersPage = new OrdersPage(driver);
     }
     @AfterClass
     public static void cleanup() {
