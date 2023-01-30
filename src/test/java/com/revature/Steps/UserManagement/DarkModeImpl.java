681a15a56884825f9b3f2e669a3b72904f064633
leibniz12 <frejusniz@gmail.com>
2023-01-30 17:59:47 -0500

    added implementation of dark mmode, login and registration

--- src/test/java/com/revature/Steps/UserManagement/DarkModeImpl.java
+++ src/test/java/com/revature/Steps/UserManagement/DarkModeImpl.java
@@ -3,38 +3,72 @@ package com.revature.Steps.UserManagement;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static com.revature.runners.UserManagementRunnerL.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class DarkModeImpl {
+    WebElement checkbox = driver.findElement(By.cssSelector("input[type='checkbox']"));
 
     @Given("user or admin is logged in using {string} and {string}")
     public void user_admin_is_logged_in_using_and(String string, String string2) {
-        throw new io.cucumber.java.PendingException();
+        driver.get("http://localhost:3000/login");
+        loginPage.emailField.sendKeys(string);
+        loginPage.passwordField.sendKeys(string2);
+        loginPage.signInButton.click();
     }
     @Given("the dark mode is off")
     public void the_dark_mode_is_off() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        // Locate the checkbox input that the switch is based on
+        WebElement checkbox = driver.findElement(By.cssSelector("input[type='checkbox']"));
+
+        // Check the value of the checkbox
+        if (checkbox.isSelected()) {
+            // Deselect the checkbox to turn off the dark mode
+            checkbox.click();
+        }
     }
-    @When("the dark mode switch is toggled")
-    public void the_dark_mode_switch_is_toggled() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @Given("the dark mode is on")
+    public void the_dark_mode_is_on() {
+        // Check the value of the checkbox
+        if (!checkbox.isSelected()) {
+            // Select the checkbox to turn on the dark mode
+            checkbox.click();
+        }
     }
-    @Then("dark mode should be turned on")
-    public void dark_mode_should_be_turned_on() {
+
+    @Given("no user is logged in and the dark mode is off")
+    public void no_user_is_logged_in_and_the_dark_mode_is_off() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("dark mode should be turned off")
-    public void dark_mode_should_be_turned_off() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.get("http://localhost:3000");
     }
 
     @Given("no user is logged in and the dark mode is on")
     public void no_user_is_logged_in_and_the_dark_mode_is_on() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.get("http://localhost:3000");
+    }
+    @When("the dark mode switch is toggled")
+    public void the_dark_mode_switch_is_toggled() {
+        // Write code here that turns the phrase above into concrete actions
+
+        checkbox.click();
+    }
+    @Then("dark mode should be turned on")
+    public void dark_mode_should_be_turned_on() {
+        // Write code here that turns the phrase above into concrete actions
+        assertTrue(checkbox.isSelected());
+    }
+    @Then("dark mode should be turned off")
+    public void dark_mode_should_be_turned_off() {
+        // Write code here that turns the phrase above into concrete actions
+        assertFalse(checkbox.isSelected());
     }
 
 }
--- src/test/java/com/revature/Steps/UserManagement/RegisterNegativeImpl.java
+++ src/test/java/com/revature/Steps/UserManagement/RegisterNegativeImpl.java
@@ -1,4 +1,16 @@
 package com.revature.Steps.UserManagement;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static com.revature.runners.UserManagementRunnerL.registerPage;
+import static org.junit.Assert.assertTrue;
+
 public class RegisterNegativeImpl {
+
+    @Then("the user should still be on the register page")
+    public void the_user_should_still_be_on_the_register_page() {
+        // Write code here that turns the phrase above into concrete actions
+        assertTrue(registerPage.firstNameField.isDisplayed());
+    }
 }
