7badb3ba24819bcd552d01f1c518e469589f23ac
Skye Gibney <skyejgibney@gmail.com>
2023-02-02 08:55:20 -0800

    Use MailTrap instead of Gmail for reset password steps
    * Less wait time during tests
    * Not dependent on UI of another software (uses MailTrap API), which also makes the tests
    run more reliably

--- src/test/java/com/revature/features/UserManagement/ResetPassword-Negative.feature
+++ src/test/java/com/revature/features/UserManagement/ResetPassword-Negative.feature
@@ -1,21 +1,18 @@
 Feature: Reset Password - Negative
-  Scenario: User resets password with invalid password
+  Scenario Outline: User resets password with invalid password
     Given the user is on the login page
     When  the user clicks on the reset your password link
     Then  the user should be on the forgot password page
     When  the user enters their email address into the email address text input
     And   the user clicks on the send reset password link button
     Then  a message should appear containing "A link to reset your password has been sent"
-    When  the user goes to their email
-    Then  the user should see an unread email with the title Reset your Congo Password
-    When  the user clicks on the email
-    And   the user clicks on the provided link
+    When  the user goes to their email and clicks on the sent link
     Then  the user should be on the reset password page
     When  the user enters "<password>" into the new password input
     And   the user clicks on the reset password button
     Then  the user should not be on the login page
     When  the user enters their new credentials into the form and clicks on the sign in button
-    Then  the user should be not be on the home page after resetting their password
+    Then  the user should not be on the home page after resetting their password
 
     Examples:
       | password   |
--- src/test/java/com/revature/helperfunctions/HelperFunctions.java
+++ src/test/java/com/revature/helperfunctions/HelperFunctions.java
@@ -1,5 +1,8 @@
 package com.revature.helperfunctions;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.StaleElementReferenceException;
@@ -9,6 +12,11 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Duration;
 import java.util.*;
 
@@ -55,4 +63,116 @@ public class HelperFunctions {
         return m;
     }
 
+    public static List<Email> getEmails() {
+        try {
+            URL obj = new URL("https://mailtrap.io/api/accounts/1470429/inboxes/2084130/messages");
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("API-Token", "d6ef45e0d4812c01773811016a747492");
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                List<Email> messages = objectMapper.readValue(response.toString(), new TypeReference<List<Email>>() {});
+                return messages;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String getLastLineEmailContent(long emailId) {
+        try {
+            URL obj = new URL("https://mailtrap.io/api/accounts/1470429/inboxes/2084130/messages/" + emailId + "/body.txt");
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("API-Token", "d6ef45e0d4812c01773811016a747492");
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                String response = "";
+
+                while ((inputLine = in.readLine()) != null) {
+                    response = inputLine;
+                }
+                in.close();
+
+                return response;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static int getNumberOfEmails() {
+        List<Email> emails = getEmails();
+
+        if (emails == null) {
+            return 0;
+        }
+
+        return emails.size();
+    }
+
+    public static String getMostRecentEmailLink() {
+        List<Email> emails = getEmails();
+
+        if (emails == null || emails.size() == 0)
+            return null;
+
+        return getLastLineEmailContent(emails.get(0).getId());
+    }
+}
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+class Email {
+    private long id;
+    private long inbox_id;
+    private String subject;
+    private String from_email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getInbox_id() {
+        return inbox_id;
+    }
+
+    public void setInbox_id(long inbox_id) {
+        this.inbox_id = inbox_id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getFrom_email() {
+        return from_email;
+    }
+
+    public void setFrom_email(String from_email) {
+        this.from_email = from_email;
+    }
 }
